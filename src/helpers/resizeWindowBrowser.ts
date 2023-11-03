09d8f67094f304386c75e72a27ca80e63dc043e3
AleksandrSmirnov92 <ryan00@mail.ru>
2023-11-03 09:33:29 +0300

    remove all files

--- src/helpers/resizeWindowBrowser.ts
+++
@@ -1,9 +0,0 @@
-import { Rectangle } from "../models/classRectangle";
-export function resizeWindowBrowser(
-  canvas: HTMLCanvasElement,
-  rectangleList: Rectangle[]
-) {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  rectangleList.forEach((item) => item.draw(canvas, 1));
-}
--- src/types/index.ts
+++
@@ -1,8 +0,0 @@
-export interface RectangleInfoForSaveTypes {
-  id: number;
-  x: number;
-  y: number;
-  color: number[];
-  text: string;
-  textSize: number;
-}
